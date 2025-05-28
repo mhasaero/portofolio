9028414057d815cca6444de56fdca0a56bea5cfd
mhasaero <eroouji7314@gmail.com>
2025-05-28 23:02:16 +0700

    fix: app routing

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,16 @@
 import { ReactNode } from "react";
 import Navbar from "@/components/Navbar";
+import Login from "@/components/Login";
 
-type Props = {
-  children: ReactNode;
-};
+export default function MainLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const user = true;
 
-export default function MainLayout({ children }: Props) {
+  if (!user) return <Login />;
+
   return (
     <main className="px-8 md:px-12 lg:px-16">
       <nav className="-mx-8 md:-mx-12 lg:-mx-16">
